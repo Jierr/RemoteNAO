34409b71086b1155dabee41bbf4f5d77c18c5a74
Pierre Bockner <pierre.bockner@gmail.com>
2012-11-19 18:57:33 +0100

    Test Server 01

--- remoteServer/main.cpp
+++ remoteServer/main.cpp
@@ -1,12 +1,61 @@
-/*
- * Copyright (c) 2012 Aldebaran Robotics. All rights reserved.
- * Use of this source code is governed by a BSD-style license that can be
- * found in the COPYING file.
- */
-#include <iostream>
+//#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
 
 int main()
 {
-  std::cout << "Hello, world" << std::endl;
-  return 0;
+	char msg[255]={0,};
+	int status = 0;
+	struct in_addr ip;
+	char ipStr[20]= {0,};
+	unsigned short int port = 0;
+	struct addrinfo hints;
+	struct addrinfo *servinfo;
+	struct sockaddr_storage incomming;
+	socklen_t addr_size;
+
+	int sserver = 0;
+	int sclient = 0;
+	int optval = 1;
+
+	memset (&hints, 0, sizeof(struct addrinfo));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+	
+	status = getaddrinfo(NULL, "32768", &hints, &servinfo);
+//	printf("IP Address for host:\r\n");
+	ip = ((struct sockaddr_in*)servinfo->ai_addr)->sin_addr;
+	port = ((struct sockaddr_in*)servinfo->ai_addr)->sin_port;
+	inet_ntop(servinfo->ai_family, &ip, ipStr, 20);
+//	printf("%s:%d\r\n", ipStr, ntohs(port));
+//	printf("Get a Socket...\r\n");
+	sserver = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+	setsockopt(sserver, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int));
+//	printf("Bind to Port...\r\n");
+	bind(sserver, servinfo->ai_addr, servinfo->ai_addrlen);
+
+	while (1)
+	{
+//		printf("Listen...\r\n");
+		listen(sserver, 1);
+		addr_size = sizeof(incomming);
+		sclient = accept(sserver, (struct sockaddr*)&incomming, &addr_size);
+//		printf("Verbindung akzeptiert...\r\n");
+//		printf("Sende Message...\r\n");
+		strcpy(msg, "[Remote Server NAO] Willkommen\r\n");
+		send(sclient, msg, strlen(msg),0);
+		close(sclient);
+		close(sserver);
+		freeaddrinfo(servinfo);
+		exit(0);
+	}
+	freeaddrinfo(servinfo);
+	return 0;
 }
